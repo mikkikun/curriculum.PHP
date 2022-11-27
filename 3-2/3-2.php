8ddba716097352d248ace139bda43b078928af23
mikkikun <nattukun.1111@icloud.com>
2022-11-27 20:34:13 +0900

    課題提出テスト
    3-2の提出

--- 3-2/3-2.php
+++ 3-2/3-2.php
@@ -4,11 +4,30 @@
   define("TAXT", 1.1);
 
   //②商品の情報を連想配列に入れましょう。
-  $products = ["鉛筆" => a, "消しゴム" => b, "物差し" => c];
+  $products = ["鉛筆" => 100, "消しゴム" => 150, "物差し" => 500];
+  $a = $products["鉛筆"];
+  $b = $products["消しゴム"];
+  $c = $products["物差し"];
+
 
   //③税率を計算する関数を用意します。
   //引数には値段を受け取り、税込価格を返答します。
+  function money($d) {
+    return $d * TAXT;
+  }
+
+  $e = money($a);
+  $f = money($b);
+  $g = money($c);
+  $products = ["鉛筆" => $e, "消しゴム" => $f, "物差し" => $g];
 
 
   //④繰り返し文を使って画面に指定の文字を表示しましょう！
+  foreach ($products as $key => $value) {
+    echo $key;
+    echo "の税込み価格は";
+    echo $value;
+    echo "です";
+    echo "<br />";
+  } 
   ?>
