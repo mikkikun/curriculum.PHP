a119ffe926b62c2f9d147f9225689806de73ea75
mikkikun <nattukun.1111@icloud.com>
2022-12-01 17:28:22 +0900

    課題提出
    4-2提出

--- 4-1/bugTest/test2/index.php
+++ 4-1/bugTest/test2/index.php
@@ -1,8 +1,8 @@
 <?php
-require 'dbconnect.php';
+// require 'dbconnect.php';
 // セッション開始
 session_start();
-include_once("dbInfo.php");
+include_once("dbconnect.php");
 
 // エラーメッセージ、登録完了メッセージの初期化
 $errorMessage = "";
@@ -34,7 +34,7 @@ if (isset($_POST["signUp"])) {
         try {
             $pdo = new PDO($dsn, $db['user'], $db['pass'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
-            $stmt = $pdo->prepare("INSERT INTO userData(name, password) VALUES (?, ?)");
+            $stmt = $pdo->prepare("INSERT INTO users(name, password) VALUES (?, ?)");
 
             $stmt->execute(array($username, password_hash($password, PASSWORD_DEFAULT)));  // パスワードのハッシュ化を行う（今回は文字列のみなのでbindValue(変数の内容が変わらない)を使用せず、直接excuteに渡しても問題ない）
             $userid = $pdo->lastinsertid();  // 登録した(DB側でauto_incrementした)IDを$useridに入れる
@@ -42,8 +42,8 @@ if (isset($_POST["signUp"])) {
             $signUpMessage = '登録が完了しました。あなたの登録IDは ' . $userid . ' です。パスワードは ' . $password . ' です。';  // ログイン時に使用するIDとパスワード
         } catch (PDOException $e) {
             $errorMessage = 'データベースエラー';
-            // $e->getMessage() でエラー内容を参照可能（デバック時のみ表示）
-            // echo $e->getMessage();
+            $e->getMessage();
+            echo $e->getMessage();
         }
     } else if ($_POST["password"] != $_POST["password2"]) {
         $errorMessage = 'パスワードに誤りがあります。';
---
+++ 4-2/index.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Y&I Group Inc</title>
+    </head>
+    <body>
+      
+      <div class = "header">
+        <div class = "logo">
+          <img src = "1599315827_logo.png" alt="">
+        </div>
+        <div class = "userinfo">
+          <div class = "userinfo-top">
+            <div class = "background-top">
+              <div class = "name">ようこそさん</div>
+            </div>
+          </div>
+          <div class = "background-bottom">
+            <div class = "background-bottom">
+            <div class = "time">最終ログイン日：</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class = "main">
+        <table>
+          <tr class = "line-title">
+            <th class = "line-name">記事ID</th>
+            <th class = "line-name">タイトル</th>
+            <th class = "line-name">カテゴリー</th>
+            <th class = "line-name">本文</th>
+            <th class = "line-name">投稿日</th>
+          </tr>
+          <tr class = "line1">
+            <th class = "line">記事ID</th>
+            <th class = "line">タイトル</th>
+            <th class = "line">カテゴリー</th>
+            <th class = "line">本文</th>
+            <th class = "line">投稿日</th>
+          </tr>
+          </tr>
+          <tr class = "line2">
+            <th class = "line">記事ID</th>
+            <th class = "line">タイトル</th>
+            <th class = "line">カテゴリー</th>
+            <th class = "line">本文</th>
+            <th class = "line">投稿日</th>
+          </tr>
+          </tr>
+          <tr class = "line3">
+            <th class = "line">記事ID</th>
+            <th class = "line">タイトル</th>
+            <th class = "line">カテゴリー</th>
+            <th class = "line">本文</th>
+            <th class = "line">投稿日</th>
+          </tr>
+          </tr>
+          <tr class = "line4">
+            <th class = "line">記事ID</th>
+            <th class = "line">タイトル</th>
+            <th class = "line">カテゴリー</th>
+            <th class = "line">本文</th>
+            <th class = "line">投稿日</th>
+          </tr>
+          <tr class = "line5">
+            <th class = "line">記事ID</th>
+            <th class = "line">タイトル</th>
+            <th class = "line">カテゴリー</th>
+            <th class = "line">本文</th>
+            <th class = "line">投稿日</th>
+          </tr>
+        </table>
+      </div>
+      <div class = "footer">
+        <div class = "footer-background">
+          <div class = "inc">Y&I group.inc</div>
+        </div>
+      </div>
+
+
+    </body>
+</html>
