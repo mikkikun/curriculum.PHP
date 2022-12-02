19f1d56a199799831805c1c056066a7f64ac0faf
mikkikun <nattukun.1111@icloud.com>
2022-12-02 21:55:35 +0900

    課題提出
    4-2提出

--- 4-2/index.php
+++ 4-2/index.php
@@ -1,25 +1,50 @@
+<?php
+require 'getData.php';
+
+$getData = new getData();
+$user = $getData->getUserData();
+$post = $getData->getPostData();
+$first_name = $user['first_name'];
+$last_name = $user['last_name'];
+$time = $user["last_login"];
+if ($value["category_no"] == 1) {
+  $num = "食事";
+}elseif($value["category_no"] == 2) {
+    $num = "旅行";
+}else {
+    $num = "その他";
+}
+
+// foreach($post as $data => $value){
+//   echo $data["id"];
+//   echo $data["title"];
+//   echo $data["category_no"];
+//   echo $data["comment"];
+//   echo $data["created"];
+//   echo '<br>';
+// }
+
+?>
+
 <!doctype html>
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
         <title>Y&I Group Inc</title>
     </head>
     <body>
-      
+
       <div class = "header">
         <div class = "logo">
-          <img src = "1599315827_logo.png" alt="">
+          <img class = "img" src = "1599315827_logo.png" alt="">
         </div>
         <div class = "userinfo">
           <div class = "userinfo-top">
-            <div class = "background-top">
-              <div class = "name">ようこそさん</div>
-            </div>
+            <div class = "name">ようこそ <?php echo $last_name; ?><?php echo $first_name; ?>さん</div>
           </div>
-          <div class = "background-bottom">
-            <div class = "background-bottom">
-            <div class = "time">最終ログイン日：</div>
-            </div>
+          <div class = "userinfo-bottom">
+            <div class = "time">最終ログイン日：<?php echo $time; ?></div>
           </div>
         </div>
       </div>
@@ -33,50 +58,26 @@
             <th class = "line-name">本文</th>
             <th class = "line-name">投稿日</th>
           </tr>
-          <tr class = "line1">
-            <th class = "line">記事ID</th>
-            <th class = "line">タイトル</th>
-            <th class = "line">カテゴリー</th>
-            <th class = "line">本文</th>
-            <th class = "line">投稿日</th>
-          </tr>
-          </tr>
-          <tr class = "line2">
-            <th class = "line">記事ID</th>
-            <th class = "line">タイトル</th>
-            <th class = "line">カテゴリー</th>
-            <th class = "line">本文</th>
-            <th class = "line">投稿日</th>
-          </tr>
-          </tr>
-          <tr class = "line3">
-            <th class = "line">記事ID</th>
-            <th class = "line">タイトル</th>
-            <th class = "line">カテゴリー</th>
-            <th class = "line">本文</th>
-            <th class = "line">投稿日</th>
-          </tr>
-          </tr>
-          <tr class = "line4">
-            <th class = "line">記事ID</th>
-            <th class = "line">タイトル</th>
-            <th class = "line">カテゴリー</th>
-            <th class = "line">本文</th>
-            <th class = "line">投稿日</th>
-          </tr>
-          <tr class = "line5">
-            <th class = "line">記事ID</th>
-            <th class = "line">タイトル</th>
-            <th class = "line">カテゴリー</th>
-            <th class = "line">本文</th>
-            <th class = "line">投稿日</th>
+        <?php foreach($post as $data => $value){ ?>
+          <tr class = "lines">
+            <th class = "line"><?php echo $value["id"]; ?></th>
+            <th class = "line"><?php echo $value["title"]; ?></th>
+            <th class = "line"><?php  if ($value["category_no"] == 1) {
+                                        echo $num = "食事";
+                                      }elseif($value["category_no"] == 2) {
+                                        echo  $num = "旅行";
+                                      }else {
+                                        echo  $num = "その他";
+                                      }?></th>
+            <th class = "line"><?php echo $value["comment"]; ?></th>
+            <th class = "line"><?php echo $value["created"]; ?></th>
+        <?php } ?>
           </tr>
         </table>
       </div>
+      
       <div class = "footer">
-        <div class = "footer-background">
-          <div class = "inc">Y&I group.inc</div>
-        </div>
+        <div class = "inc">Y&I group.inc</div>
       </div>
 
 
